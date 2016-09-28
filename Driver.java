f8f0e5b5b184503992fdfa4f8c5afed8857a63f8
tdobeck17 <tdobeck17@jcu.edu>
2016-09-28 16:36:20 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -13,7 +13,8 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		try
+		{
 			//this is based on Tim's code
 			Scanner sc = new Scanner(System.in);
 			System.out.print("Enter the number for the size of the board: ");
@@ -21,6 +22,10 @@ public class Driver {
 			System.out.println("Please indicate if you wish to start with a checkerboard by typing \"1\" or start randomly by typing \"0\": ");
 			int typeBoard = sc.nextInt();
 	
+			if (typeBoard > 1)
+			{
+				throw new Exception();
+			}
 			//as Tim noted, we should have a try/catch here, but the long term goal is not to need the scanner actually
 
 			sc.close();
@@ -31,13 +36,10 @@ public class Driver {
 			frame.add(board);
 			frame.pack();
 			frame.setVisible(true);
-			
-			
-		
-
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Invalid respose")
+		}
 	}
-
-
-	
-
 }
