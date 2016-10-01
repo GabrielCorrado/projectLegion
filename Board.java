f469f1c8325e65cfbf3ac0dd67ae30791028ff01
Nick Corrado <gcorrado17@jcu.edu>
2016-10-01 14:36:45 -0400

    Update Board.java
    Fixed indentation from the end of the constructor onwards. This is what was causing 90% of the merge conflict: indentation is part of the line, so GitHub though we'd changed hundreds of lines of code. Please please please always run Correct Indentation on your code by selecting all of it and hitting ctrl+I (that's a capital i) in Eclipse.

--- Board.java
+++ Board.java
@@ -87,285 +87,284 @@ public class Board extends JPanel implements MouseInputListener {
 
 }
 
-protected void layer2(Color polarity)
-{
-	
-	cells2 = new Cell_2[size][size];
-	
-	
-	for (int row = 0; row < cells.length; row++) {
-		for (int col = 0; col < cells[row].length; col++) {
+	protected void layer2(Color polarity)
+	{
 
-			if(polarity == Color.RED)
-				//if the top left is black
-			{
-				if(cells[row][col].getColor() == Color.BLACK)
-					//if the layer 1 cell is black
+		cells2 = new Cell_2[size][size];
+
+
+		for (int row = 0; row < cells.length; row++) {
+			for (int col = 0; col < cells[row].length; col++) {
+
+				if(polarity == Color.RED)
+					//if the top left is black
 				{
-					if(col%2 == row%2)
-						//if its in a spot that should be black
+					if(cells[row][col].getColor() == Color.BLACK)
+						//if the layer 1 cell is black
 					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
+						if(col%2 == row%2)
+							//if its in a spot that should be black
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
 							//then you are the same polarity as cell[0][0]
-					}
-					else
-						//if its in a spot that SHOULDN'T be black
-					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						}
+						else
+							//if its in a spot that SHOULDN'T be black
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
 							//then you are in the opposite polarity than cells[0][0]
+						}
 					}
-				}
-				else
-					//if the layer 1 cell is white
-				{
-					if(col%2 == row%2)
-						//if its in a spot that SHOULDN'T be 
+					else
+						//if the layer 1 cell is white
 					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						if(col%2 == row%2)
+							//if its in a spot that SHOULDN'T be 
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
 							// then its in the opposite polarity than cells[0]
-					}
-					else
-						//if its in a spot that should be white
-					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
+						}
+						else
+							//if its in a spot that should be white
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
 							//then its in the same polarity as cells[0][0]
-					}
-				}
-			}
-			else
-			{
-				if(cells[row][col].getColor() == Color.WHITE)
-				{
-					if(col%2 == row%2)
-					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
-					}
-					else
-					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						}
 					}
 				}
 				else
 				{
-					if(col%2 == row%2)
+					if(cells[row][col].getColor() == Color.WHITE)
 					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						if(col%2 == row%2)
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
+						}
+						else
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						}
 					}
 					else
 					{
-						cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
+						if(col%2 == row%2)
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.BLUE);
+						}
+						else
+						{
+							cells2[row][col] = new Cell_2(800+EXTRA_BOARD_SPACE+row*cellSize, EXTRA_BOARD_SPACE+col*cellSize, cellSize, Color.RED);
+						}
 					}
 				}
 			}
 		}
 	}
-}
 
 
-protected void paintComponent(Graphics arg0) {
-	super.paintComponent(arg0);
+	protected void paintComponent(Graphics arg0) {
+		super.paintComponent(arg0);
 
-	Graphics2D g = (Graphics2D)arg0;
-	//Graphics2D g2 = (Graphics2D)arg0;
+		Graphics2D g = (Graphics2D)arg0;
+		//Graphics2D g2 = (Graphics2D)arg0;
 
-	for (int row = 0; row < size*2; row++) {
-		for (int col = 0; col <size; col++) {
-			if (row<size)
-			{
-				display[row][col] = cells[row][col];
+		for (int row = 0; row < size*2; row++) {
+			for (int col = 0; col <size; col++) {
+				if (row<size)
+				{
+					display[row][col] = cells[row][col];
+				}
+				else
+				{
+					display[row][col] = cells2[row-size][col];
+				}
+				display[row][col].draw(g);
 			}
-			else
-			{
-				display[row][col] = cells2[row-size][col];
-			}
-			display[row][col].draw(g);
 		}
-	}
-	
-	/*for (int row = 0; row < cells2.length; row++) {
+
+		/*for (int row = 0; row < cells2.length; row++) {
 			for (int col = 0; col < cells2[row].length; col++) {
 				cells2[row][col].draw(g2);
-
 			}
 		}*/
-}
+	}
 
-public void step() {
-	//initialize temp array of colors
-	Color[][] result = new Color[size][size];
-	for (int row = 0; row < cells.length; row++) {
-		for (int col = 0; col < cells[row].length; col++) {
-			int tally = 0;
-			//check how many neighbors are alive
-			for (Cell n : getNeighbors(cells, row, col)) {
-				//the array may have nulls in it ;)
-				if (n != null) {
-					if (n.getColor() == Color.BLACK) {
-						tally++;
+	public void step() {
+		//initialize temp array of colors
+		Color[][] result = new Color[size][size];
+		for (int row = 0; row < cells.length; row++) {
+			for (int col = 0; col < cells[row].length; col++) {
+				int tally = 0;
+				//check how many neighbors are alive
+				for (Cell n : getNeighbors(cells, row, col)) {
+					//the array may have nulls in it ;)
+					if (n != null) {
+						if (n.getColor() == Color.BLACK) {
+							tally++;
+						}
 					}
 				}
-			}
-			//if three of your neighbors are alive, you come alive or stay alive,
-			//or if two of your neighbors are alive and you're alive, you stay alive,
-			//otherwise you die
-			if (tally == 3 || (tally == 2 && cells[row][col].getColor() == Color.BLACK)) {
-				//result[row][col] = new Cell(cells[row][col].getX(), cells[row][col].getY(), cells[row][col].getWidth(), Color.BLACK);
-				result[row][col] = Color.BLACK;
-			}
-			else {
-				//result[row][col] = new Cell(cells[row][col].getX(), cells[row][col].getY(), cells[row][col].getWidth(), Color.WHITE);
-				result[row][col] = Color.WHITE;
+				//if three of your neighbors are alive, you come alive or stay alive,
+				//or if two of your neighbors are alive and you're alive, you stay alive,
+				//otherwise you die
+				if (tally == 3 || (tally == 2 && cells[row][col].getColor() == Color.BLACK)) {
+					//result[row][col] = new Cell(cells[row][col].getX(), cells[row][col].getY(), cells[row][col].getWidth(), Color.BLACK);
+					result[row][col] = Color.BLACK;
+				}
+				else {
+					//result[row][col] = new Cell(cells[row][col].getX(), cells[row][col].getY(), cells[row][col].getWidth(), Color.WHITE);
+					result[row][col] = Color.WHITE;
+				}
 			}
 		}
-	}
 
-	//populate the old array with the results of the temp array
-	for (int row = 0; row < cells.length; row++) {
-		for (int col = 0; col < cells[row].length; col++) {
-			cells[row][col].setColor(result[row][col]);
+		//populate the old array with the results of the temp array
+		for (int row = 0; row < cells.length; row++) {
+			for (int col = 0; col < cells[row].length; col++) {
+				cells[row][col].setColor(result[row][col]);
+			}
 		}
-	}
-	layer2(polarity);
-}
-
-public Cell[] getNeighbors(Cell[][] cells, int rowNum, int colNum) {
-	//each cell only has 8 neighbors! for now at least.... :(
-	Cell[] neighbors = new Cell[8];
-	//this is pretty cool
-	int rowMax = cells.length-1;
-	int colMax = cells[rowMax-1].length-1;
-
-	//THIS IS AWFUL AND NEEDS SO MUCH REVISION IT'S NOT EVEN FUNNY
-
-	//top left
-	if (rowNum == 0 && colNum == 0) {
-		neighbors[4] = cells[rowNum][colNum+1];
-		neighbors[6] = cells[rowNum+1][colNum];
-		neighbors[7] = cells[rowNum+1][colNum+1];
+		layer2(polarity);
 	}
 
-	//bottom left
-	if (rowNum == rowMax && colNum == 0) {
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[2] = cells[rowNum-1][colNum+1];
-		neighbors[4] = cells[rowNum][colNum+1];
+	public Cell[] getNeighbors(Cell[][] cells, int rowNum, int colNum) {
+		//each cell only has 8 neighbors! for now at least.... :(
+		Cell[] neighbors = new Cell[8];
+		//this is pretty cool
+		int rowMax = cells.length-1;
+		int colMax = cells[rowMax-1].length-1;
+
+		//THIS IS AWFUL AND NEEDS SO MUCH REVISION IT'S NOT EVEN FUNNY
+
+		//top left
+		if (rowNum == 0 && colNum == 0) {
+			neighbors[4] = cells[rowNum][colNum+1];
+			neighbors[6] = cells[rowNum+1][colNum];
+			neighbors[7] = cells[rowNum+1][colNum+1];
+		}
+
+		//bottom left
+		if (rowNum == rowMax && colNum == 0) {
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[2] = cells[rowNum-1][colNum+1];
+			neighbors[4] = cells[rowNum][colNum+1];
+		}
+
+		//top right
+		if (rowNum == 0 && colNum == cells[0].length-1) {
+			neighbors[3] = cells[rowNum][colNum-1];
+			neighbors[5] = cells[rowNum+1][colNum-1];
+			neighbors[6] = cells[rowNum+1][colNum];
+		}
+
+		//bottom right
+		if (rowNum == rowMax && colNum == colMax) {
+			neighbors[0] = cells[rowNum-1][colNum-1];
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[3] = cells[rowNum][colNum-1];
+		}
+
+		//top
+		if (rowNum == 0 && colNum > 0 && colNum < colMax) {
+			neighbors[3] = cells[rowNum][colNum-1];
+			neighbors[4] = cells[rowNum][colNum+1];
+			neighbors[5] = cells[rowNum+1][colNum-1];
+			neighbors[6] = cells[rowNum+1][colNum];
+			neighbors[7] = cells[rowNum+1][colNum+1];
+		}
+
+		//bottom
+		if (rowNum == rowMax && colNum > 0 && colNum < colMax) {
+			neighbors[0] = cells[rowNum-1][colNum-1];
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[2] = cells[rowNum-1][colNum+1];
+			neighbors[3] = cells[rowNum][colNum-1];
+			neighbors[4] = cells[rowNum][colNum+1];
+		}
+
+		//left
+		if (rowNum > 0 && rowNum < rowMax && colNum == 0) {
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[2] = cells[rowNum-1][colNum+1];
+			neighbors[4] = cells[rowNum][colNum+1];
+			neighbors[6] = cells[rowNum+1][colNum];
+			neighbors[7] = cells[rowNum+1][colNum+1];
+		}
+
+		//right
+		if (rowNum > 0 && rowNum < rowMax && colNum == colMax) {
+			neighbors[0] = cells[rowNum-1][colNum-1];
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[3] = cells[rowNum][colNum-1];
+			neighbors[5] = cells[rowNum+1][colNum-1];
+			neighbors[6] = cells[rowNum+1][colNum];
+		}
+
+		//middle cells obviously get everything
+		if (rowNum > 0 && rowNum < rowMax && colNum > 0 && colNum < colMax) {
+			neighbors[0] = cells[rowNum-1][colNum-1];
+			neighbors[1] = cells[rowNum-1][colNum];
+			neighbors[2] = cells[rowNum-1][colNum+1];
+			neighbors[3] = cells[rowNum][colNum-1];
+			neighbors[4] = cells[rowNum][colNum+1];
+			neighbors[5] = cells[rowNum+1][colNum-1];
+			neighbors[6] = cells[rowNum+1][colNum];
+			neighbors[7] = cells[rowNum+1][colNum+1];
+		}
+
+		return neighbors;
 	}
 
-	//top right
-	if (rowNum == 0 && colNum == cells[0].length-1) {
-		neighbors[3] = cells[rowNum][colNum-1];
-		neighbors[5] = cells[rowNum+1][colNum-1];
-		neighbors[6] = cells[rowNum+1][colNum];
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
-	//bottom right
-	if (rowNum == rowMax && colNum == colMax) {
-		neighbors[0] = cells[rowNum-1][colNum-1];
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[3] = cells[rowNum][colNum-1];
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
-	//top
-	if (rowNum == 0 && colNum > 0 && colNum < colMax) {
-		neighbors[3] = cells[rowNum][colNum-1];
-		neighbors[4] = cells[rowNum][colNum+1];
-		neighbors[5] = cells[rowNum+1][colNum-1];
-		neighbors[6] = cells[rowNum+1][colNum];
-		neighbors[7] = cells[rowNum+1][colNum+1];
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
-	//bottom
-	if (rowNum == rowMax && colNum > 0 && colNum < colMax) {
-		neighbors[0] = cells[rowNum-1][colNum-1];
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[2] = cells[rowNum-1][colNum+1];
-		neighbors[3] = cells[rowNum][colNum-1];
-		neighbors[4] = cells[rowNum][colNum+1];
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		//i commented this out for now so you can test the step procedure by clicking
+		//the behavior is boring atm because checkerboard is a bad starting pattern (almost everything
+		//has four neighbors, so everything dies immediately, except the border cells, which all
+		//spring alive)
+		//		for (int row = 0; row < cells.length; row++) {
+		//			for (int col = 0; col < cells[row].length; col++) {
+		//				if (cells[row][col].contains(arg0.getPoint())) {
+		//					cells[row][col].flipColor();
+		//				}
+		//			}
+		//		}
+		step();
+		repaint();
 	}
 
-	//left
-	if (rowNum > 0 && rowNum < rowMax && colNum == 0) {
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[2] = cells[rowNum-1][colNum+1];
-		neighbors[4] = cells[rowNum][colNum+1];
-		neighbors[6] = cells[rowNum+1][colNum];
-		neighbors[7] = cells[rowNum+1][colNum+1];
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
-	//right
-	if (rowNum > 0 && rowNum < rowMax && colNum == colMax) {
-		neighbors[0] = cells[rowNum-1][colNum-1];
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[3] = cells[rowNum][colNum-1];
-		neighbors[5] = cells[rowNum+1][colNum-1];
-		neighbors[6] = cells[rowNum+1][colNum];
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
 	}
 
-	//middle cells obviously get everything
-	if (rowNum > 0 && rowNum < rowMax && colNum > 0 && colNum < colMax) {
-		neighbors[0] = cells[rowNum-1][colNum-1];
-		neighbors[1] = cells[rowNum-1][colNum];
-		neighbors[2] = cells[rowNum-1][colNum+1];
-		neighbors[3] = cells[rowNum][colNum-1];
-		neighbors[4] = cells[rowNum][colNum+1];
-		neighbors[5] = cells[rowNum+1][colNum-1];
-		neighbors[6] = cells[rowNum+1][colNum];
-		neighbors[7] = cells[rowNum+1][colNum+1];
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
-
-	return neighbors;
-}
-
-@Override
-public void mouseClicked(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mouseEntered(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mouseExited(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mousePressed(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	//i commented this out for now so you can test the step procedure by clicking
-	//the behavior is boring atm because checkerboard is a bad starting pattern (almost everything
-	//has four neighbors, so everything dies immediately, except the border cells, which all
-	//spring alive)
-	//		for (int row = 0; row < cells.length; row++) {
-	//			for (int col = 0; col < cells[row].length; col++) {
-	//				if (cells[row][col].contains(arg0.getPoint())) {
-	//					cells[row][col].flipColor();
-	//				}
-	//			}
-	//		}
-	step();
-	repaint();
-}
-
-@Override
-public void mouseReleased(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mouseDragged(MouseEvent e) {
-	// TODO Auto-generated method stub
-}
-
-@Override
-public void mouseMoved(MouseEvent e) {
-	// TODO Auto-generated method stub
-
-}
 }
