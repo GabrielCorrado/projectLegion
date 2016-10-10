18d90715deeb5a21df827e1c38073b82fc313986
tdobeck17 <tdobeck17@jcu.edu>
2016-10-10 16:14:57 -0400

    Update GenCell.java
    Commented

--- GenCell.java
+++ GenCell.java
@@ -1,3 +1,11 @@
+/*		Author: Zak Gray and Tim Dobeck
+ * 		Description: GenCell allows for the objects of Cell and Cell_2 to be created and the rectangles (cells) to be drawn. This extends Rectangle2D and uses the
+ * 					 Rectangle2D superclass for creating the cells.
+ * 		Parameters: GenCell is the constructor. Each cell has an X coordinate, Y coordinate, fixed size, and Cell is either black or white while Cell_2 is either red or blue
+ * 					Draw is allowing the graphics to appear on the JPanel
+ * 
+ */
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
@@ -7,6 +15,7 @@ public class GenCell extends Rectangle2D.Double {
 	protected double size;
 	protected Color c;
 	
+	//constructor
 	public GenCell(double x, double y, double size, Color c)
 	{
 		super(x,y,size,size);
@@ -25,4 +34,3 @@ public class GenCell extends Rectangle2D.Double {
 		g.fill(this);
 		g.fillRect((int)x, (int)y, (int)size, (int)size);
 	}
-}
