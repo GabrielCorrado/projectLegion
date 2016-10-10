8929bb9a89bbe7d835912a796923f994374e315c
tdobeck17 <tdobeck17@jcu.edu>
2016-10-10 16:11:29 -0400

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -1,3 +1,14 @@
+/*		Author: Nick Corrado and Tim Dobeck
+ * 		Description: This is the constructor class for the Agents to be created and drawn. Velocity determined the direction each agent will move and
+ * 		Color is for determining if the color of the agent will be green or invisible. The rest of the agents are being made because it is extending the
+ * 		Rectangle2D superclass.
+ * 		Parameters: Agent is made up of its x coordinate, y coordinate, fixed size of agent, Point2D is the direction the agent will move, and color of agent
+ * 					Draw is made up of Graphics g which is the type for drawing things in a JPanel
+ * 					SetColor is made up of the specific color of the agent, this is called in the Board class
+ * 					Both setVelocity's determine a new x and y coordinate for the Agents to have after they step
+ * 					SetX and SetY determines where each agent will be placed originally
+ */
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
@@ -5,16 +16,18 @@ import java.awt.geom.Point2D;
 
 public class Agent extends Ellipse2D.Double {
 	private int size;
-	private Point2D velocity;
-	private Color color;
-	public boolean agentPastBoard = false;
+	private Point2D velocity;	//adds direction to our agents
+	private Color color; 	//only adding a color here so we can make it green or invisible in the board class
+	public boolean agentPastBoard = false;    //helps determine if the color will be green or invisible
 	
+	//constructor
 	public Agent(int x, int y, int size, Point2D v, Color color) {
 		super(x, y, size, size);
 		this.velocity = v;
 		this.color = color;
 	}
 	
+	//draws Agents using the superclass for Ellipse2D
 	public void draw(Graphics2D g) {
 		g.setColor(color);
 		g.fill(this);
@@ -33,16 +46,18 @@ public class Agent extends Ellipse2D.Double {
 		return velocity;
 	}
 	
+	//determines how much an agent will move in a particular direction
 	public void setVelocity(Point2D velocity) {
 		this.velocity = new Point2D.Double(velocity.getX(), velocity.getY());
 	}
 	
+	//determines how much an agent will move in a particular direction
 	public void setVelocity(double x, double y) {
 		this.velocity = new Point2D.Double(x, y);
 	}
 	
 	public void setX(double x) {
-		//i'm autocasting this to an int before i give it b/c i don't want to deal with fractional bullshit
+		//this is being autocast to an int in board
 		this.x = x;
 	}
 	
