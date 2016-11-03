287fa4a0461f3f5d23e190d4aba20f44d7e9051d
tdobeck17 <tdobeck17@jcu.edu>
2016-11-03 16:03:05 -0400

    Update LabelHandler.java

--- other/LabelHandler.java
+++ other/LabelHandler.java
@@ -1,5 +1,11 @@
-package other;
-
+package code;
+/*
+ * CURRENTLY NOT USED
+ * Author: Zak Gray
+ * Description: This is an object to be passed from board to GUI so that labels in GUI can be updated
+ * Parameters: The initial board size(nXn), the initial number of Agents, the initial White cells in board.cells and 
+ * the initial black cells in board.cells
+ */
 public class LabelHandler {
 	private int initBoardSize, initSwarmCount, initWhiteCellCount, initBlackCellCount;
 	
@@ -10,9 +16,38 @@ public class LabelHandler {
 		this.initWhiteCellCount = initWhiteCellCount;
 		this.initBlackCellCount = initBlackCellCount;
 	}
-	
+	//getters
 	public int getInitBoardSize()
 	{
 		return initBoardSize;
+	}
+	public int getInitSwarmCount()
+	{
+		return initSwarmCount;
+	}
+	public int getInitWhiteCellCount()
+	{
+		return initWhiteCellCount;
+	}
+	public int getInitBlackCellCount()
+	{
+		return initBlackCellCount;
+	}
+	//setters
+	public void setInitBoardSize(int size)
+	{
+		initBoardSize = size;
+	}
+	public void setInitSwarmCount(int swarmCount)
+	{
+		initSwarmCount = swarmCount;
+	}
+	public void setInitWhiteCellCount(int whiteCount)
+	{
+		initWhiteCellCount = whiteCount;
+	}
+	public void setInitBlackCellCount(int blackCount)
+	{
+		initBlackCellCount = blackCount;
 	}
 }
