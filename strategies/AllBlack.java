acb7665b69f127dd2187fe39002f52f6d91afd6a
Nick Corrado <gcorrado17@jcu.edu>
2016-11-30 16:23:18 -0500

    Changed the way SwarmAgent's velocity is set: it's based on cellSize now, not on a hardcoded pixel value. Swapped where variables are initialized to speed up MouseDragged. Fixed some bad indentation in Board and AllBlack.

--- strategies/AllBlack.java
+++ strategies/AllBlack.java
@@ -15,34 +15,34 @@ public class AllBlack extends AbstractStrategy{
 		for (int row = 0; row < layer1.length; row++) {
 			for (int col = 0; col < layer1[row].length; col++) {
 
-					if(layer1[row][col].getColor() == Color.BLACK)
-						//if the layer 1 cell is black
-					{
-						layer2[row][col] = new Cell(row*cellSize, col*cellSize, cellSize, GUI.getPolarity1());
-					}
-					else
-						//if the layer 1 cell is white
-					{
-						layer2[row][col] = new Cell(row*cellSize, col*cellSize, cellSize, GUI.getPolarity2());
-					}
+				if(layer1[row][col].getColor() == Color.BLACK)
+					//if the layer 1 cell is black
+				{
+					layer2[row][col] = new Cell(row*cellSize, col*cellSize, cellSize, GUI.getPolarity1());
 				}
-			
+				else
+					//if the layer 1 cell is white
+				{
+					layer2[row][col] = new Cell(row*cellSize, col*cellSize, cellSize, GUI.getPolarity2());
+				}
+			}
+
 		}
 		return layer2;
 	}
 
 	@Override
 	public void logic(SwarmAgent agent, Cell[][] layer1, Cell[][] layer2, GenericCell[] neighbors, Cell cell, int cellSize) {
-	
-				if(layer1[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].getColor() == Color.BLACK)
-				{
-				}
-				else
-				{
-					layer1[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].flipColor();
-					layer2[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].flipColor();
-				}
-		
+
+		if(layer1[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].getColor() == Color.BLACK)
+		{
+		}
+		else
+		{
+			layer1[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].flipColor();
+			layer2[(int)agent.getCenterX()/cellSize][(int)agent.getCenterY()/cellSize].flipColor();
+		}
+
 	}
 
 }
